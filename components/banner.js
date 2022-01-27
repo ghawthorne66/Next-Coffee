b0804d147f0605de68763ed5dbcaf73535b531ce
ghawthorne66 <ghawthorne@me.com>
2022-01-27 13:34:12 +0100

    add global styling

--- components/banner.js
+++ components/banner.js
@@ -1,15 +1,20 @@
-import styles from "../styles/Banner.module.css"
+import styles from "../styles/Banner.module.css";
 
-const Banner = ({buttonText, handleOnClick}) => {
+const Banner = (props) => {
   return (
-      <div className={styles.container}>
-        <h1 className={styles.title}>
-          <span className={styles.title1}>Coffee </span>
-          <span className={styles.title2}>Connoisseur</span>
-        </h1>
-        <p className={styles.subtitle}>Discover your local coffee shops!</p>
-        <button onClick={handleOnClick} className={styles.button}>{buttonText}</button>
+    <div className={styles.container}>
+      <h1 className={styles.title}>
+        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title2}>Connoisseur</span>
+      </h1>
+      <p className={styles.subTitle}>Discover your local coffee shops!</p>
+      <div className={styles.buttonWrapper}>
+        <button className={styles.button} onClick={props.handleOnClick}>
+          {props.buttonText}
+        </button>
       </div>
+    </div>
+  );
+};
 
-  )}
-export default Banner
+export default Banner;
