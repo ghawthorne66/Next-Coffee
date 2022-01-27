d93627258ac51a96e7f86dcd557ceaa4a0df5d74
ghawthorne66 <ghawthorne@me.com>
2022-01-27 13:21:21 +0100

    first commit

---
+++ components/banner.js
@@ -0,0 +1,15 @@
+import styles from "../styles/Banner.module.css"
+
+const Banner = ({buttonText, handleOnClick}) => {
+  return (
+      <div className={styles.container}>
+        <h1 className={styles.title}>
+          <span className={styles.title1}>Coffee </span>
+          <span className={styles.title2}>Connoisseur</span>
+        </h1>
+        <p className={styles.subtitle}>Discover your local coffee shops!</p>
+        <button onClick={handleOnClick} className={styles.button}>{buttonText}</button>
+      </div>
+
+  )}
+export default Banner
